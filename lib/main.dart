05cb8f6e1125dd84ed458152185ab62a60f63877
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-06-04 14:16:22 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Product UIs',
       theme: ThemeData(
+        // backgroundColor: const Color.fromARGB(255, 43, 41, 41),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- lib/productUis/product_one.dart
+++ lib/productUis/product_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class product_one extends StatefulWidget {
   const product_one({super.key});
@@ -8,6 +9,16 @@ class product_one extends StatefulWidget {
 }
 
 class _product_oneState extends State<product_one> {
+  final lightTheme = ThemeData(
+    primaryColor: Colors.grey,
+    hintColor: const Color.fromARGB(255, 43, 41, 41),
+    focusColor: Color.fromARGB(237, 0, 0, 0),
+    errorColor: Color.fromARGB(255, 0, 0, 0),
+    hoverColor: Color.fromARGB(255, 0, 0, 0),
+    fontFamily: 'Roboto',
+    // Add more theme properties as needed
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +28,7 @@ class _product_oneState extends State<product_one> {
             children: [
               IconButton(
                 onPressed: () {},
-                color: Colors.grey,
+                color: lightTheme.primaryColor,
                 icon: Icon(Icons.sort),
               ),
               Expanded(
@@ -42,14 +53,14 @@ class _product_oneState extends State<product_one> {
             ],
           ),
         ),
-        backgroundColor: const Color.fromARGB(255, 43, 41, 41),
+        backgroundColor: Color.fromARGB(255, 43, 41, 41),
       ),
       body: Container(
-        color: Color.fromARGB(237, 0, 0, 0),
+        color: Color.fromARGB(255, 0, 0, 0),
         child: ListView(
           children: [
             Container(
-              color: Color.fromARGB(255, 0, 0, 0),
+              color: Color.fromARGB(255, 43, 41, 41),
               child: Row(
                 children: [
                   Expanded(
@@ -57,7 +68,9 @@ class _product_oneState extends State<product_one> {
                           onPressed: () {},
                           child: Text(
                             "Discover",
-                            style: TextStyle(color: Colors.grey),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
                           ))),
                   Expanded(
                       child: TextButton(
@@ -69,23 +82,68 @@ class _product_oneState extends State<product_one> {
                 ],
               ),
             ),
-            Card(
-              margin: EdgeInsets.all(7),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+            gradientCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class gradientCard extends StatelessWidget {
+  const gradientCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(7),
+      elevation: 4,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Container(
+          width: 60,
+          height: 170,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "assets/images/grtwall.jpg",
               ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.pink.withOpacity(0.6),
+                Colors.orange.withOpacity(0.6),
+              ],
+            )),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 36),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.book),
+                  Icon(
+                    Icons.church_outlined,
+                    color: Colors.white,
+                    size: 27,
+                  ),
                   SizedBox(
                     height: 10,
                   ),
-                  Text("Nature's Light")
+                  Text(
+                    "Nature's Light",
+                    style: TextStyle(color: Colors.white),
+                  )
                 ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
