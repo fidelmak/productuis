db3d2adfd62e6b255ef36be007e57c62d4ddd05b
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-06-24 10:23:38 +0100

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:productuis/responso/responso.dart';
 import './productUis/constants.dart';
+import 'freeres/courses_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
             ),
-            home: HomePage() //product_one() // //,
+            home: CoursesPage() // HomePage() //product_one() // //,
             );
       },
     );
